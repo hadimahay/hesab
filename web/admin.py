96d03db331515dd9198fe926e97641c26230e8be
hadimahay <hadimahay@gmail.com>
2021-09-30 14:53:12 +0330

    expense model is there

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Expense
 # Register your models here.
+
+@admin.register(Expense)
+class Expens_admin(admin.ModelAdmin):
+    list_display = ['text','amount','date','user']
+    TimeField = ('date',)
+    search_fields = ['amount']
